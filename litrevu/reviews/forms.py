7f1d755d9935b0194283f71ac8ea583e3f8d41ec
Morgan Prieur <79154432+morganprieur@users.noreply.github.com>
2024-04-18 18:24:17 +0200

    Update forms.py
    Add missing Docstrings.

--- litrevu/reviews/forms.py
+++ litrevu/reviews/forms.py
@@ -1,14 +1,16 @@
 
+# reviews/forms.py 
 from django import forms 
 from django.contrib.auth import get_user_model 
 from django.contrib.auth.forms import UserCreationForm 
 from django.forms import ModelForm 
 
-# reviews/forms.py
 from . import models
 
 
-class SignupForm(UserCreationForm):
+class SignupForm(UserCreationForm): 
+    """ Class that displays only the useful fields to register a user. 
+    """ 
     class Meta(UserCreationForm.Meta):
         model = get_user_model()
         fields = ( 
@@ -20,10 +22,14 @@ class SignupForm(UserCreationForm):
 
 
 class UserForm(forms.Form): 
+    """ General class to search a user on his/her username. 
+    """ 
     username = forms.CharField(label="pseudo ", max_length=150) 
 
 
 class TicketForm(ModelForm): 
+    """ Class that displays the fields to register one ticket. 
+    """ 
     class Meta: 
         model = models.Ticket 
         fields = [ 
@@ -34,6 +40,9 @@ class TicketForm(ModelForm):
 
 
 class NewReviewForm(ModelForm): 
+    """ Class that displays the fields to register a new review and ticket at the same time. 
+        The body of the review is displayed at a textarea. 
+    """ 
     class Meta: 
         model = models.Review 
         fields = [ 
@@ -51,6 +60,9 @@ class NewReviewForm(ModelForm):
 
 
 class ReviewForm(ModelForm): 
+    """ Class that displays the fields to register a new review, answering to a ticket. 
+    The body of the review is displayed at a textarea, and the ticket input is hidden. 
+    """ 
     class Meta: 
         model = models.Review 
         fields = [ 
